595a6146dcc6d4e2e23f4f4ab86d208acd62587e
Gustavo Tomás <guga.tomas10@gmail.com>
2022-08-25 20:55:35 -0300

    formatted code

--- trab2.py
+++ trab2.py
@@ -1,10 +1,11 @@
 # Part 1 -> Generate a 1024 bit prime
 
-import random, math
+import random
+import math
 
 # Generate a random number between 2^(n-1) + 1 and 2^(n) - 1 -> avoid small primes
 def genBit(n):
-	return random.randrange(2**(n-1) + 1, 2**(n) - 1)
+  return random.randrange(2**(n-1) + 1, 2**(n) - 1)
 
 # List of the first few primes
 primesList = []
@@ -13,50 +14,51 @@ primesList = []
 def genPrimesList(n):
   prime = [True for i in range(n + 1)]
   p = 2
-  
+
   while p * p <= n:
     if prime[p] == True:
-      for i in range(p * p, n + 1, p): prime[i] = False
+      for i in range(p * p, n + 1, p):
+        prime[i] = False
     p += 1
-  
+
   for p in range(2, n+1):
-    if prime[p]: primesList.append(p)
+    if prime[p]:
+      primesList.append(p)
 
 # Tests if generated number of n bits is prime
-def getLowLevelPrime(n):
+def getPossiblePrime(n):
   while True:
     primeCandidate = genBit(n)
     for divisor in primesList:
       if primeCandidate % divisor == 0 and divisor**2 <= primeCandidate:
         break
-      else: 
+      else:
         return primeCandidate
 
 # Run the Miller-Rabin test n times
 def passesMillerRabin(possiblePrime, n):
 
-	maxDivisionsByTwo = 0
-	evenComponent = possiblePrime-1
+  maxDivisionsByTwo = 0
+  evenComponent = possiblePrime-1
 
-	while evenComponent % 2 == 0:
-		evenComponent >>= 1
-		maxDivisionsByTwo += 1
-	assert(2**maxDivisionsByTwo * evenComponent == possiblePrime-1)
+  while evenComponent % 2 == 0:
+    evenComponent >>= 1
+    maxDivisionsByTwo += 1
+  assert (2**maxDivisionsByTwo * evenComponent == possiblePrime-1)
 
-	def trialComposite(roundTester):
-		if pow(roundTester, evenComponent, possiblePrime) == 1:
-			return False
-		for i in range(maxDivisionsByTwo):
-			if pow(roundTester, 2**i * evenComponent, possiblePrime) == possiblePrime - 1:
-				return False
-		return True
+  def trialComposite(roundTester):
+    if pow(roundTester, evenComponent, possiblePrime) == 1:
+      return False
+    for i in range(maxDivisionsByTwo):
+      if pow(roundTester, 2**i * evenComponent, possiblePrime) == possiblePrime - 1:
+        return False
+    return True
 
-	for i in range(n + 1):
-		roundTester = random.randrange(2, possiblePrime)
-		if trialComposite(roundTester):
-			return False
-	return True
-
+  for i in range(n + 1):
+    roundTester = random.randrange(2, possiblePrime)
+    if trialComposite(roundTester):
+      return False
+  return True
 
 def main():
   # Generates the first few primes
@@ -64,8 +66,8 @@ def main():
 
   # Generates a Miller-Rabin approved prime
   while True:
-    number = getLowLevelPrime(1024)   # Gets possible prime
-    if passesMillerRabin(number, 20): # Runs the MR test with n rounds
+    number = getPossiblePrime(1024)   # Gets possible prime
+    if passesMillerRabin(number, 20):  # Runs the MR test with n rounds
       print("MR:", number)
       break
 
@@ -74,5 +76,6 @@ def main():
   testPrime = number.getPrime(1024)
   print("TEST:", testPrime)
 
-if __name__ == "__main__": main()
 
+if __name__ == "__main__":
+    main()
